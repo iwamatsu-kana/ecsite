554244d01f6dfb5d220fd4cdbe25c17ae152f204
iwamatsu-kana <2828cdmpsakura@au.com>
2024-03-31 20:46:03 +0900

    修正しました。

--- ecsite/src/com/diworksdev/ecsite/dao/BuyItemCompleteDAO.java
+++ ecsite/src/com/diworksdev/ecsite/dao/BuyItemCompleteDAO.java
@@ -3,19 +3,18 @@ package com.diworksdev.ecsite.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
 import com.diworksdev.ecsite.util.DBConnector;
 import com.diworksdev.ecsite.util.DateUtil;
 
 public class BuyItemCompleteDAO {
 
-		public void buyItemeInfo(String item_transaction_id, String user_master_id, String
-total_price, String total_count, String pay) throws SQLException {
+public void buyItemeInfo(String item_transaction_id, String user_master_id, String total_price, String total_count, String pay) throws SQLException {
+
 		DBConnector dbConnector = new DBConnector();
 		Connection connection = dbConnector.getConnection();
 		DateUtil dateUtil = new DateUtil();
-		String sql = "INSERT INTO user_buy_item_transaction"
-				(item_transaction_id, total_ptice, total_count, user_master_id, pay, insert_date)
-				VALUES(?,?,?,?,?,?)";
+		String sql = "INSERT INTO user_buy_item_transaction(item_transaction_id, total_ptice, total_count, user_master_id, pay, insert_date)VALUES(?,?,?,?,?,?)";
 
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
