878b37bdbc794e5a4f82911872fa57a6c14a81a0
iwamatsu-kana <2828cdmpsakura@au.com>
2024-04-07 18:33:15 +0900

    修正しました。

--- ecsite/src/com/diworksdev/ecsite/dao/MyPageDAO.java
+++ ecsite/src/com/diworksdev/ecsite/dao/MyPageDAO.java
@@ -14,11 +14,11 @@ public class MyPageDAO {
 			private DBConnector dbConnector=new DBConnector();
 			private Connection connection=dbConnector.getConnection();
 
-			public ArrayList<MyPageDTO> getMyPageUserInfo
-			(String item_transaction_id, String user_master_id) throws SQLException
-			{ ArrayList<MyPageDTO> myPageDTO = new ArrayList<MyPageDTO>();
+			public ArrayList<MyPageDTO> getMyPageUserInfo(String item_transaction_id, String user_master_id)
+throws SQLException{
+				ArrayList<MyPageDTO> myPageDTO = new ArrayList<MyPageDTO>();
 
-			String sql="SELECT ubit.id,iit.item_name,ubit.total_price,ubit.total_count,ubit.pay,ubit.insert_date FROM user_buy_item_transaction ubit LEFT JOIN item_info_transaction iit ON ubit.item_transaction_id = iit.id WHERE ubit.item_transaction_id = ? AND ubit.user_master_id = ? ORDER BY ubit.insert_date DESC";
+			String sql="SELECT ubit.id,iit.item_name,ubit.total_price,ubit.total_count,ubit.pay,ubit.insert_date FROM user_buy_item_transaction ubit LEFT JOIN item_info_transaction iit ON ubit.item_transaction_id = iit.id WHERE ubit.item_transaction_id = ? AND ubit.user_master_id = ? ORDER BY insert_date DESC";
 
 			try {
 				PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -32,7 +32,7 @@ public class MyPageDAO {
 				dto.setItemName(resultSet.getString("item_name"));
 				dto.setTotalPrice(resultSet.getString("total_price"));
 				dto.setTotalCount(resultSet.getString("total_count"));
-				dto.setPayment(resultSet.getString("pay")) ;
+				dto.setPayment(resultSet.getString("pay"));
 				dto.setInsert_date(resultSet.getString("insert_date"));
 				myPageDTO.add(dto);
 			}
